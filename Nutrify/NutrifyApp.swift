f4c89813fcad6c5cbb58414a3ba9ea2f800a9768
micchan7 <michellechan0077@gmail.com>
2024-11-27 01:42:52 +0800

    Update NutrifyApp.swift

--- Nutrify/NutrifyApp.swift
+++ Nutrify/NutrifyApp.swift
@@ -10,11 +10,19 @@ import SwiftUI
 @main
 struct NutrifyApp: App {
     let persistenceController = PersistenceController.shared
+     @StateObject var nutritionData = NutritionDataModel()
+
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            TabView {
+                HomeView(nutritionData: nutritionData)
+                    .tabItem { Label("Home", systemImage: "house") }
+                LogPageView(nutritionData: nutritionData)
+                    .tabItem { Label("Log", systemImage: "doc.plaintext") }
+            }
         }
     }
 }
